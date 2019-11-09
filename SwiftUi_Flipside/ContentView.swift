b4cc27094611e1bd48953ee7a3ae353805668323
Larry Shannon <larry@breakawaydesign.com>
2019-11-09 12:50:12 -0800

    Initial Commit

---
+++ SwiftUi_Flipside/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUi_Flipside
+//
+//  Created by Larry Shannon on 11/9/19.
+//  Copyright © 2019 Larry Shannon. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
